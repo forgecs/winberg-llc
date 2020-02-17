f7722689ebb05b299b532b5bd1b3979a9ca368e1
Andrew Gilliland <andrewpgilliland@gmail.com>
2020-02-17 12:02:05 -0600

    added about section on landing page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -32,9 +32,53 @@ export const IndexPageTemplate = ({
         <h3 className="text-blue-grey-050 text-lg sm:text-xl px-2 py-1 mt-5">
           {subheading}
         </h3>
-        <Link className="text-blue-grey-050 hover:text-blue-grey-900 bg-blue-grey-900 hover:bg-white mt-8 px-8 py-2" to="/contact">
+        <Link
+          className="text-blue-grey-050 hover:text-blue-grey-900 bg-blue-grey-900 hover:bg-white mt-8 px-8 py-2"
+          to="/contact"
+        >
           Free Consultation
         </Link>
+      </div>
+    </section>
+
+    <section className="max-w-4xl text-blue-grey-900 text-center mx-auto px-4 py-16">
+      <h2 className="font-serif text-blue-grey-900 text-2xl sm:text-4xl">
+        About Winberg LLC
+      </h2>
+
+      <div className="flex flex-col sm:flex-row mt-10">
+
+        <div className="bg-white shadow-lg mx-1 px-2 py-10">
+          <h3 className="font-serif text-lg sm:text-xl">
+            Trusted Legal Support
+          </h3>
+          <p className="italic mt-5">
+            We know that this can sometimes be a difficult and overwhelming
+            process. We are dedicated to guiding you towards a strategic legal
+            solution.{" "}
+          </p>
+        </div>
+
+        <div className="bg-white shadow-lg mx-1 px-2 py-10 mt-10 sm:mt-0">
+          <h3 className="font-serif text-lg sm:text-xl">
+            Focus on Real Results
+          </h3>
+          <p className="italic mt-5">
+            The first step is for us to work with you and develop a plan
+            tailored specifically for your needs. Through our understanding of
+            the law and your understanding of the circumstances, we will search
+            for an equitable solution.
+          </p>
+        </div>
+
+        <div className="bg-white shadow-lg mx-1 px-2 py-10 mt-10 sm:mt-0">
+          <h3 className="font-serif text-lg sm:text-xl">Get Started Today</h3>
+          <p className="italic mt-5">
+            Contact us for a free phone consultation. The sooner you have a plan
+            of action, the better your chances of taking the correct steps to
+            get the results you want. Se Habla Espanol.{" "}
+          </p>
+        </div>
       </div>
     </section>
 
